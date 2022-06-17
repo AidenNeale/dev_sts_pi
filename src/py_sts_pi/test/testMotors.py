9eb356bbb88f99397c6567c33e812be38561a401
AidenNeale <aidenneale@sky.com>
2022-06-17 18:14:38 +0100

    Slow speed

--- src/py_sts_pi/test/testMotors.py
+++ src/py_sts_pi/test/testMotors.py
@@ -2,7 +2,7 @@ import explorerhat
 import time
 
 if __name__=='__main__':
-    explorerhat.motor.one.forwards(100)
-    explorerhat.motor.two.backwards(100)
+    explorerhat.motor.one.forwards(50)
+    explorerhat.motor.two.backwards(50)
     while True:
         pass
