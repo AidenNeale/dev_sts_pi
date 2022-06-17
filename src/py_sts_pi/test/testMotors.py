6457ae0393dbcb1cc6c53f6cdec3aa70aac061e6
AidenNeale <aidenneale@sky.com>
2022-06-17 18:11:53 +0100

    idiot

--- src/py_sts_pi/test/testMotors.py
+++ src/py_sts_pi/test/testMotors.py
@@ -3,6 +3,6 @@ import time
 
 if __name__=='__main__':
     explorerhat.motor.one.forwards(100)
-    explorerhat.motor.one.backwards(100)
+    explorerhat.motor.two.backwards(100)
     while True:
         pass
