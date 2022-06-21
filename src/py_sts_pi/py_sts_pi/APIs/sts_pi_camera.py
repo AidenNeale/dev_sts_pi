23a590f307cab92753f50dfe0f6bd9ed1f55606f
AidenNeale <aidenneale@sky.com>
2022-06-21 15:03:36 +0100

    Added appropriate resolution

--- src/py_sts_pi/py_sts_pi/APIs/sts_pi_camera.py
+++ src/py_sts_pi/py_sts_pi/APIs/sts_pi_camera.py
@@ -35,7 +35,8 @@ class ImagePublisher(Node):
     # Create a VideoCapture object
     # The argument '0' gets the default webcam.
     self.cap = cv2.VideoCapture(0)
-
+    self.cap.set(3, 480)
+    self.cap.set(4, 640)
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
 
