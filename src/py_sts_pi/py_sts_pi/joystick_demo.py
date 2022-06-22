f7acf4cf792768060b1f6ba0e06c2edcda211e1b
AidenNeale <aidenneale@sky.com>
2022-06-22 15:33:28 +0100

    Created Joystick Demo - initial commit - untested

---
+++ src/py_sts_pi/py_sts_pi/joystick_demo.py
@@ -0,0 +1,99 @@
+# Basic ROS 2 program to subscribe to real-time streaming
+# video from your built-in webcam
+# Author:
+# - Addison Sears-Collins
+# - https://automaticaddison.com
+
+# Import the necessary libraries
+import rclpy  # Python library for ROS 2
+from rclpy.node import Node  # Handles the creation of nodes
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+class FlashcardDemo(Node):
+  """
+  Create an ImageSubscriber class, which is a subclass of the Node class.
+  """
+  def __init__(self):
+    """
+    Class constructor to set up the node
+    """
+    # Initiate the Node class's constructor and give it a name
+    super().__init__('FlashcardDemo')
+
+    self.subscription = self.create_subscription(
+      Joy,
+      'joy',
+      self.listener_callback,
+      10)
+    self.subscription # prevent unused variable warning
+
+    self.publisher_ = self.create_publisher(
+      Twist,
+      'twist_motor',
+      10
+    )
+
+
+  def listener_callback(self, data):
+    """
+    Callback function.
+    """
+    # Display the message on the console
+    # self.get_logger().info('Receiving video frame')
+
+    msg = Twist()
+
+    # Convert ROS Image message to OpenCV image
+    self.joystick = data.axes # [0] = Left and Right, [1] = Up and Down
+    if (abs(self.joystick[0]) < 0.15):
+      self.stopMovement()
+    elif (abs(self.joystick[0]) >= 0.25):
+      self.combinedMovement(msg, self.joystick[1], self.joystick[0])
+    elif (abs(self.joystick[1]) >= 0.15):
+      self.combinedMovement(msg, self.joystick[1], self.joystick[0])
+    # print(self.joystick)
+    
+
+  def stopMovement(self, msg):
+    msg.linear.x = 0.0
+    msg.angular.z = 0.0
+    self.publisher_.publish(msg)
+
+  def linearMovement(self, msg, speed):
+    msg.linear.x = speed
+    msg.angular.z = 0.0
+    self.publisher_.publish(msg)
+
+  def angularMovement(self, msg, angular):
+    msg.linear.x = 1.0
+    msg.angular.z = angular
+    self.publisher_.publish(msg)
+
+  def combinedMovement(self, msg, speed, angular):
+    msg.linear.x = speed
+    msg.angular.z = angular
+    self.publisher_.publish(msg)
+
+
+def main(args=None):
+
+  # Initialize the rclpy library
+  rclpy.init(args=args)
+
+  # Create the node
+  flashcard_node = FlashcardDemo()
+
+  # Spin the node so the callback function is called.
+  rclpy.spin(flashcard_node)
+
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  flashcard_node.destroy_node()
+
+  # Shutdown the ROS client library for Python
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+  main()
