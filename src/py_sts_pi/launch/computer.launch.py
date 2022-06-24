4407026aaf5c82f02b603d5852bf8bf79df4d29b
AidenNeale <aidenneale@sky.com>
2022-06-24 16:08:12 +0100

    Added namespacefor drivers

--- src/py_sts_pi/launch/computer.launch.py
+++ src/py_sts_pi/launch/computer.launch.py
@@ -6,33 +6,44 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        # launch.actions.DeclareLaunchArgument(
-        #     'node_prefix',
-        #     default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-        #     description='Prefix for node names'),
         launch_ros.actions.Node(
-            package='py_sts_pi', 
+            package='py_sts_pi',
             executable='aruco',
-            namespace='test1',
+            namespace='driver_1',
         ),
         launch_ros.actions.Node(
             package='py_sts_pi', 
             executable='display',
-            namespace='test1',
+            namespace='driver_1',
         ),
         launch_ros.actions.Node(
             package='py_sts_pi', 
             executable='joystick',
-            namespace='test1',
+            namespace='driver_1',
         ),
         launch_ros.actions.Node(
             package='joy', 
             executable='joy_node',
-            namespace='test1',
+            namespace='driver_1',
         ),
-            # name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
-        # launch_ros.actions.Node(
-        #     package='py_sts_pi', 
-        #     executable='talker',
-            # name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
+        launch_ros.actions.Node(
+            package='py_sts_pi',
+            executable='aruco',
+            namespace='driver_2',
+        ),
+        launch_ros.actions.Node(
+            package='py_sts_pi', 
+            executable='display',
+            namespace='driver_2',
+        ),
+        launch_ros.actions.Node(
+            package='py_sts_pi', 
+            executable='joystick',
+            namespace='driver_2',
+        ),
+        launch_ros.actions.Node(
+            package='joy', 
+            executable='joy_node',
+            namespace='driver_2',
+        ),
     ])
