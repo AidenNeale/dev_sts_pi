b35936ca9762c67d2c56f5029f1b96578ac682e2
AidenNeale <aidenneale@sky.com>
2022-06-24 18:58:37 +0100

    Updated namespace to include environment variable 'ROBOT_ID' which declares the purpose of each robot

--- src/py_sts_pi/launch/sts.launch.py
+++ src/py_sts_pi/launch/sts.launch.py
@@ -6,19 +6,19 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        # launch.actions.DeclareLaunchArgument(
-        #     'node_prefix',
-        #     default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-        #     description='Prefix for node names'),
+        launch.actions.DeclareLaunchArgument(
+            'node_prefix',
+            default_value=[launch.substitutions.EnvironmentVariable('ROBOT_ID')],
+            description='Prefix for node names',
+        ),
         launch_ros.actions.Node(
             package='py_sts_pi', 
             executable='camera',
-            namespace='test1',
+            namespace=[launch.substitutions.LaunchConfiguration('node_prefix')],
         ),
         launch_ros.actions.Node(
             package='py_sts_pi', 
             executable='motors',
-            namespace='test1',
+            namespace=[launch.substitutions.LaunchConfiguration('node_prefix')],
         ),
-            # name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
     ])
