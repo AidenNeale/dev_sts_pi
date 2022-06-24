725e6d010466bdaf344a45de6df9197d93420575
AidenNeale <aidenneale@sky.com>
2022-06-24 13:17:30 +0100

    Add test launch files for separate motor control

---
+++ src/py_sts_pi/launch/sts_2.launch.py
@@ -0,0 +1,24 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        # launch.actions.DeclareLaunchArgument(
+        #     'node_prefix',
+        #     default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
+        #     description='Prefix for node names'),
+        launch_ros.actions.Node(
+            package='py_sts_pi', 
+            executable='camera',
+            namespace='test2',
+        ),
+        launch_ros.actions.Node(
+            package='py_sts_pi', 
+            executable='motors',
+            namespace='test2',
+        ),
+            # name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
+    ])
